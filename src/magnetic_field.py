bb1875746d2982e5eecea6dfbc509a7f83b147ed
Eashwar-S <eashwar.sathyamurthy@gmail.com>
2025-09-16 17:01:51 -0400

    Rectified error in magnetic field approach

--- src/magnetic_field.py
+++ src/magnetic_field.py
@@ -122,7 +122,7 @@ class MagneticFieldRouter:
         S = (1 - w) * P + w * D
         final_score = S
         if is_new_required:
-            final_scores = S + 1000
+            final_score = S + 1000
         #     final_score = 1000/total_required_edges + S  # Large bonus for uncovered required edges
         # else:
         #     final_score = S + (2*self.capacity)/(self.capacity - current_length + 1)
